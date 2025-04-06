c9a9910e9cabefd9f0272b948cda2faf3af97cf8
lonearvin <outlookddal@gmail.com>
2025-04-06 15:05:47 +0800

    2025.4.6提交

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,23 @@
 #include <algorithm>
 using namespace std;
 
+int number2(int i){
+    int res = 0;
+    while (i)
+    {
+        if (i%10 == 2) res++;
+        i /= 10;
+    }
+    return res;
+}
+
 int main(){
-
-
+    int l, r; cin>>l>>r;
+    int ans = 0;
+    for(int i=l;i<=r;i++){
+        ans+= number2(i);
+    }
+    cout<<ans<<endl;
     return 0;
 }
 // 21 32 43
